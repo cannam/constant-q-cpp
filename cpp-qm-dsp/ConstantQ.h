ea7e2393b41215aaef8baff1729e5607f84d81aa
Chris Cannam <c.cannam@qmul.ac.uk>
2013-11-05 08:41:00 +0000

    Stub class

---
+++ cpp-qm-dsp/ConstantQ.h
@@ -0,0 +1,19 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
+
+#ifndef CONSTANTQ_H
+#define CONSTANTQ_H
+
+class ConstantQ
+{
+public:
+    ConstantQ(double sampleRate, 
+	      double minFreq, double maxFreq, 
+	      int binsPerOctave);
+
+    
+
+
+}
+
+#endif
+
